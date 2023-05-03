9857afb9c192e7bfb9a1ddede6260c33ebba28bf
clrsurya11 <singhsuryakant9011@gmail.com>
2023-05-03 20:31:09 +0530

    initial commit

---
+++ Backend_Part/SupportCenter/src/main/java/com/supportcenter/SupportCenterApplication.java
@@ -0,0 +1,13 @@
+package com.supportcenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupportCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupportCenterApplication.class, args);
+	}
+
+}
---
+++ Backend_Part/SupportCenter/src/test/java/com/supportcenter/SupportCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.supportcenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupportCenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
