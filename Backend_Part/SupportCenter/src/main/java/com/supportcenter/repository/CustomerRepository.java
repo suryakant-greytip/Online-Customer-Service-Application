78f76c22c1eb83e81ac6f3b5c6066a64c2c812d4
shakambhari <shakambharihift36@gmail.com>
2023-05-04 18:17:48 +0530

    added all repos

--- Backend_Part/SupportCenter/src/main/java/com/supportcenter/repository/CustomerRepository.java
+++ Backend_Part/SupportCenter/src/main/java/com/supportcenter/repository/CustomerRepository.java
@@ -1,9 +1,33 @@
 package com.supportcenter.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer , Integer>{
 
+    public Issue addCustomerIssue(Issue issue);
+	
+	public String sendIntimationEmailToCustomer(Integer customerId , Integer issueId);
+	
+	public Issue modifyCustomerIssue(Issue issue);
+	
+	public String sentModificationEmailToCustomer(Integer customerId , Integer issueId);
+	
+	public String closeCustomerIssue(Integer issueId, Integer customerId);
+	
+	public Customer findCustomerById(Integer customerId);
+	
+	public List<Customer> findCustomerByName(String name);
+	
+	public Customer findCustomerByEmail(String email);
+	
+	public String lockCustomer(Integer customerId);
+	
+	
+	public List<Customer> findByFirstName(String firstName);
+	
+	public Customer findByEmail(String email);
 }
