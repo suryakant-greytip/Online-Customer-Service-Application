fc3aa086fcf5ed8a028010b3aea34f5bd0758f8d
Abhijeet Hiwale <abhijeethiwalemail@gmail.com>
2023-05-04 17:28:37 +0530

    DepartmentService methods done

--- Backend_Part/SupportCenter/src/main/java/com/supportcenter/service/AdminService.java
+++ Backend_Part/SupportCenter/src/main/java/com/supportcenter/service/AdminService.java
@@ -1,4 +1,27 @@
 package com.supportcenter.service;
 
+import com.supportcenter.exception.DepartmentException;
+import com.supportcenter.exception.OperatorException;
+import org.springframework.expression.spel.ast.Operator;
+
 public interface AdminService {
+    public boolean addDepartment(Department department)throws DepartmentException;
+
+    public boolean removeDepartment(Integer departmentId)throws DepartmentException;
+
+    public Department modifyDepartment(Department department)throws DepartmentException;
+
+    public Department findDepartmentById(int id)throws DepartmentException;
+
+
+    public boolean addOperator(Integer departmentID,Operator operator)throws OperatorException;
+
+    public boolean removeOperator(Integer operatorId) throws OperatorException;
+
+    public Operator modifyOperator(Operator operator)throws OperatorException;
+
+    public Operator findOperatorbyId(Integer operatorId)throws OperatorException;
+
+    public List<Operator> findAllOperator() throws OperatorException;
+
 }
