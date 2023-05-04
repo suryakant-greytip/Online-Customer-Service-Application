13e4cfdf7cdbf8ec188b7e7e1dd2493f73882faa
shakambhari <shakambharihift36@gmail.com>
2023-05-04 15:01:26 +0530

    added repos

--- Backend_Part/SupportCenter/src/main/java/com/supportcenter/repository/CustomerRepository.java
+++ Backend_Part/SupportCenter/src/main/java/com/supportcenter/repository/CustomerRepository.java
@@ -1,9 +1,39 @@
 package com.supportcenter.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CustomerRepository extends JpaRepository<Customer , Integer>{
+public interface CustomerRepository extends JpaRepository<Customer , Integer>
+{
 
+	public String registerCustomer(Customer customer);
+
+	// Customer will register himself by passing customer details.	
+	
+	
+	public Issue viewIssueById(Integer issueId,  Integer customerId);
+	
+//	Customer will validate himself using customerId and with the help of issueId he can view his issue
+	
+	public Issue reopenIssue(Integer issueId);
+//	for the issue closed if the 
+	
+	
+	public List<Issue> viewAllIssue(Integer customerId);
+	
+	
+	public String changePassword(Integer customerId , Login credentials);
+	
+	
+	public String forgotPassword(Integer customerId);
+	
+	
+	public Customer emailPassword(Integer customerId);
+	
+	public Customer findByEmail(String email);
+	
+	public List<Customer> findByFirstName(String firstName);
 }
